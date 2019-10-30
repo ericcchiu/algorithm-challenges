16e111bb81271a5f8c889062a25006f08c6570f9
ericcchiu <37917779+ericcchiu@users.noreply.github.com>
2019-10-29 20:05:32 -0500

    (FEATURE) HRATX practice problem solution 1

---
+++ HRATX/1.1_Searching for a string in an array.js
@@ -0,0 +1,30 @@
+/*
+Given a string and an array of strings, return true if the string exists in the array, 
+and false if it does not.
+
+DO NOT use .includes() in your solution.
+
+DO use a for loop.
+
+TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
+*/
+
+// const searchString = (str, arrStr) => {
+//   for (let item of arrStr) { 
+//     console.log(item); 
+//     if (str === item ) return true; 
+//   }
+//   return false
+// }
+
+const searchString = (str, arrStr) => {
+
+  if (arrStr.length === 0) return false; 
+  if(str === arrStr[0]) return true; 
+  searchString(str, arrStr.slice(2)); 
+}
+
+let str = "x"; 
+let arr = ["a", "x", "c"]; 
+
+searchString(str, arr); 
